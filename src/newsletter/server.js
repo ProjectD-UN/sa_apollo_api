eb4f5a4c7cca2ab7b5c86a43dff1ac3344d7af93
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-11-03 22:17:39 -0500

    corrigiendo error de url

--- src/newsletter/server.js
+++ src/newsletter/server.js
@@ -2,5 +2,5 @@
 // export const port = process.env.COURSES_PORT
 // export const entryPoint = process.env.COURSES_ENTRY
 
-export const url = process.env.API_URL || '35.231.237.201'
+export const url = process.env.NEWS_URL || '35.231.237.201'
 export const port_newsletter = process.env.NEWSLETTER_PORT || '3002'
