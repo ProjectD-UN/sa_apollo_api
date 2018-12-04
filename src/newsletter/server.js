9247750e5853c9b3d19841369dc618473f7b2c8b
Marlon Enrique Noguera Ramirez <36613466+menoguerar@users.noreply.github.com>
2018-12-04 02:07:46 -0500

    Update server.js

--- src/newsletter/server.js
+++ src/newsletter/server.js
@@ -1,2 +1,2 @@
-export const url = process.env.URL || '35.231.237.201'
-export const port_newsletter = process.env.NEWSLETTER_PORT || '3002'
+export const url = process.env.URL
+export const port_newsletter = process.env.NEWSLETTER_PORT
