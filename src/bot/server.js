dba6b435a8ffd438ff467a54594322cf4bdf8f6e
Marlon Enrique Noguera Ramirez <36613466+menoguerar@users.noreply.github.com>
2018-12-04 02:06:28 -0500

    Update server.js

--- src/bot/server.js
+++ src/bot/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.URL || '35.231.237.201'
-export const port_bot = process.env.BOT_PORT || '3000'
-export const entryPoint_bot = process.env.BOT_ENTRY || 'api/'
+export const url = process.env.URL
+export const port_bot = process.env.BOT_PORT
+export const entryPoint_bot = process.env.BOT_ENTRY
