394a12af4d17276406488b7e5231a5d6e22d2969
Marlon Enrique Noguera Ramirez <36613466+menoguerar@users.noreply.github.com>
2018-12-04 02:08:24 -0500

    Update server.js

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.URL || '35.231.237.201'
-export const user_port = process.env.USER_PORT || '3003'
-export const entryPoint_user = process.env.USER_ENTRY || 'api/auth'
+export const url = process.env.URL
+export const user_port = process.env.USER_PORT
+export const entryPoint_user = process.env.USER_ENTRY
