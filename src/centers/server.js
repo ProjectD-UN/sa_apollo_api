efc81f0877c9db4bb42750aa95d2d714f48d7354
Marlon Enrique Noguera Ramirez <36613466+menoguerar@users.noreply.github.com>
2018-12-04 15:13:58 -0500

    Update server.js

--- src/centers/server.js
+++ src/centers/server.js
@@ -1,3 +1,3 @@
 export const entryPoint = process.env.CENTERS_ENTRY
-export const url = process.env.URL
+export const url = process.env.CENTERS_URL
 export const port_centers = process.env.CENTERS_PORT
